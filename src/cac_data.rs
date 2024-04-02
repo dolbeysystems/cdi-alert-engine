74c3924f122e97d55d1a83ff6c4021f798bfa43b
Evie <ebennett@dolbey.com>
2024-04-02 18:58:54 -0400

    Expose all types and fields to lua

--- src/cac_data.rs
+++ src/cac_data.rs
@@ -3,6 +3,7 @@ use mongodb::{bson::doc, options::FindOneAndDeleteOptions};
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 
+// TODO: Since the account structure is read-only, fields should be Rc to avoid cloning by lua.
 #[serde_as]
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 pub struct Account {
@@ -45,7 +46,7 @@ impl mlua::UserData for Account {
         fields.add_field_method_get("discharge_date_time", |_, this| {
             Ok(this.discharge_date_time.map(|x| x.to_string()))
         });
-        // TODO: fields.add_field_method_get("patient", |_, this| Ok(this.patient));
+        fields.add_field_method_get("patient", |_, this| Ok(this.patient.clone()));
         fields.add_field_method_get("patient_type", |_, this| Ok(this.patient_type.clone()));
         fields.add_field_method_get("admit_source", |_, this| Ok(this.admit_source.clone()));
         fields.add_field_method_get("admit_type", |_, this| Ok(this.admit_type.clone()));
@@ -53,10 +54,12 @@ impl mlua::UserData for Account {
             Ok(this.hospital_service.clone())
         });
         fields.add_field_method_get("building", |_, this| Ok(this.building.clone()));
-        // TODO: fields.add_field_method_get("documents", |_, this| Ok(this.documents));
-        // TODO: fields.add_field_method_get("medications", |_, this| Ok(this.medications));
-        // TODO: fields.add_field_method_get("discrete_values", |_, this| Ok(this.discrete_values));
-        // TODO: fields.add_field_method_get("cdi_alerts", |_, this| Ok(this.cdi_alerts));
+        fields.add_field_method_get("documents", |_, this| Ok(this.documents.clone()));
+        fields.add_field_method_get("medications", |_, this| Ok(this.medications.clone()));
+        fields.add_field_method_get("discrete_values", |_, this| {
+            Ok(this.discrete_values.clone())
+        });
+        fields.add_field_method_get("cdi_alerts", |_, this| Ok(this.cdi_alerts.clone()));
     }
 }
 
@@ -78,6 +81,19 @@ pub struct Patient {
     pub birthdate: Option<DateTime<Utc>>,
 }
 
+impl mlua::UserData for Patient {
+    fn add_fields<'lua, F: mlua::UserDataFields<'lua, Self>>(fields: &mut F) {
+        fields.add_field_method_get("mrn", |_, this| Ok(this.mrn.clone()));
+        fields.add_field_method_get("first_name", |_, this| Ok(this.first_name.clone()));
+        fields.add_field_method_get("middle_name", |_, this| Ok(this.middle_name.clone()));
+        fields.add_field_method_get("last_name", |_, this| Ok(this.last_name.clone()));
+        fields.add_field_method_get("gender", |_, this| Ok(this.gender.clone()));
+        fields.add_field_method_get("birthdate", |_, this| {
+            Ok(this.birthdate.map(|x| x.to_string()))
+        });
+    }
+}
+
 #[serde_as]
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 pub struct CACDocument {
@@ -91,6 +107,19 @@ pub struct CACDocument {
     pub code_references: Option<Vec<CodeReference>>,
     #[serde(rename = "AbstractionReferences")]
     pub abstraction_references: Option<Vec<CodeReference>>,
+}
+impl mlua::UserData for CACDocument {
+    fn add_fields<'lua, F: mlua::UserDataFields<'lua, Self>>(fields: &mut F) {
+        fields.add_field_method_get("document_id", |_, this| Ok(this.document_id.clone()));
+        fields.add_field_method_get("document_type", |_, this| Ok(this.document_type.clone()));
+        fields.add_field_method_get("content_type", |_, this| Ok(this.content_type.clone()));
+        fields.add_field_method_get("code_references", |_, this| {
+            Ok(this.code_references.clone())
+        });
+        fields.add_field_method_get("abstraction_references", |_, this| {
+            Ok(this.abstraction_references.clone())
+        });
+    }
 }
 
 #[serde_as]
@@ -116,6 +145,23 @@ pub struct Medication {
     pub category: Option<String>,
 }
 
+impl mlua::UserData for Medication {
+    fn add_fields<'lua, F: mlua::UserDataFields<'lua, Self>>(fields: &mut F) {
+        fields.add_field_method_get("external_id", |_, this| Ok(this.external_id.clone()));
+        fields.add_field_method_get("medication", |_, this| Ok(this.medication.clone()));
+        fields.add_field_method_get("dosage", |_, this| Ok(this.dosage.clone()));
+        fields.add_field_method_get("route", |_, this| Ok(this.route.clone()));
+        fields.add_field_method_get("start_date", |_, this| {
+            Ok(this.start_date.map(|x| x.to_string()))
+        });
+        fields.add_field_method_get("end_date", |_, this| {
+            Ok(this.end_date.map(|x| x.to_string()))
+        });
+        fields.add_field_method_get("status", |_, this| Ok(this.status.clone()));
+        fields.add_field_method_get("category", |_, this| Ok(this.category.clone()));
+    }
+}
+
 #[serde_as]
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
 pub struct DiscreteValue {
@@ -128,6 +174,17 @@ pub struct DiscreteValue {
     #[serde(rename = "ResultDate")]
     #[serde_as(as = "Option<bson::DateTime>")]
     pub result_date: Option<DateTime<Utc>>,
+}
+
+impl mlua::UserData for DiscreteValue {
+    fn add_fields<'lua, F: mlua::UserDataFields<'lua, Self>>(fields: &mut F) {
+        fields.add_field_method_get("unique_id", |_, this| Ok(this.unique_id.clone()));
+        fields.add_field_method_get("name", |_, this| Ok(this.name.clone()));
+        fields.add_field_method_get("result", |_, this| Ok(this.result.clone()));
+        fields.add_field_method_get("result_date", |_, this| {
+            Ok(this.result_date.map(|x| x.to_string()))
+        });
+    }
 }
 
 #[serde_as]
@@ -143,6 +200,16 @@ pub struct CodeReference {
     pub start: Option<i32>,
     #[serde(rename = "Length")]
     pub length: Option<i32>,
+}
+
+impl mlua::UserData for CodeReference {
+    fn add_fields<'lua, F: mlua::UserDataFields<'lua, Self>>(fields: &mut F) {
+        fields.add_field_method_get("code", |_, this| Ok(this.code.clone()));
+        fields.add_field_method_get("description", |_, this| Ok(this.description.clone()));
+        fields.add_field_method_get("phrase", |_, this| Ok(this.phrase.clone()));
+        fields.add_field_method_get("start", |_, this| Ok(this.start.clone()));
+        fields.add_field_method_get("length", |_, this| Ok(this.length.clone()));
+    }
 }
 
 #[serde_as]
