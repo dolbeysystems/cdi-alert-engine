93bb84f0b5b89b520be9d5428c1568bcf2341dff
Michael Lambert <mlambert125@live.com>
2024-12-09 09:44:13 -0500

    Updated skeletons on unfinished

--- scripts/heart_failure.lua
+++ scripts/heart_failure.lua
@@ -19,6 +19,7 @@ local links = require("libs.common.basic_links")
 local codes = require("libs.common.codes")
 local dates = require("libs.common.dates")
 local discrete = require("libs.common.discrete_values")
+local headers = require("libs.common.headers")
 
 
 
@@ -41,76 +42,27 @@ if not existing_alert or not existing_alert.validated then
     --- Header Variables and Helper Functions
     --------------------------------------------------------------------------------
     local result_links = {}
-    local documented_dx_header = links.make_header_link("Documented Dx")
-    local documented_dx_links = {}
-    local laboratory_studies_header = links.make_header_link("Laboratory Studies")
-    local laboratory_studies_links = {}
-    local clinical_evidence_header = links.make_header_link("Clinical Evidence")
-    local clinical_evidence_links = {}
-    local treatment_and_monitoring_header = links.make_header_link("Treatment and Monitoring")
-    local treatment_and_monitoring_links = {}
+    local documented_dx_header = headers.make_header_builder("Documented Dx", 1)
+    local alert_trigger_header = headers.make_header_builder("Alert Trigger", 2)
+    local laboratory_studies_header = headers.make_header_builder("Laboratory Studies", 3)
+    local vital_signs_intake_header = headers.make_header_builder("Vital Signs/Intake", 4)
+    local clinical_evidence_header = headers.make_header_builder("Clinical Evidence", 5)
+    local treatment_and_monitoring_header = headers.make_header_builder("Treatment and Monitoring", 6)
 
-    --- @param link CdiAlertLink?
-    local function add_documented_dx_link(link)
-        table.insert(documented_dx_links, link)
-    end
-    --- @param link CdiAlertLink?
-    local function add_lab_study_link(link)
-        table.insert(laboratory_studies_links, link)
-    end
-    --- @param text string
-    local function add_lab_study_text(text)
-        table.insert(laboratory_studies_links, links.make_header_link(text))
-    end
-    --- @param link CdiAlertLink?
-    local function add_clinical_evidence_link(link)
-        table.insert(clinical_evidence_links, link)
-    end
-    --- @param code string
-    --- @param text string
-    local function add_clinical_evidence_code(code, text)
-        add_clinical_evidence_link(links.get_code_link { code = code, text = text })
-    end
-    --- @param prefix string
-    --- @param text string
-    local function add_clinical_evidence_code_prefix(prefix, text)
-        add_clinical_evidence_link(codes.get_code_prefix_link { prefix = prefix, text = text })
-    end
-    --- @param code_set string[]
-    --- @param text string
-    local function add_clinical_evidence_any_code(code_set, text)
-        add_clinical_evidence_link(links.get_code_link { codes = code_set, text = text })
-    end
-    --- @param code string
-    --- @param text string
-    local function add_clinical_evidence_abstraction(code, text)
-        add_clinical_evidence_link(links.get_abstraction_link { code = code, text = text })
-    end
-    --- @param link CdiAlertLink?
-    local function add_treatment_and_monitoring_link(link)
-        table.insert(treatment_and_monitoring_links, link)
-    end
     local function compile_links()
-        if #documented_dx_header.links > 0 then
-            table.insert(result_links, documented_dx_header)
-        end
-        if #laboratory_studies_links > 0 then
-            laboratory_studies_header.links = laboratory_studies_links
-            table.insert(result_links, laboratory_studies_header)
-        end
-        if #clinical_evidence_links > 0 then
-            clinical_evidence_header.links = clinical_evidence_links
-            table.insert(result_links, clinical_evidence_header)
-        end
-        if #treatment_and_monitoring_links > 0 then
-            treatment_and_monitoring_header.links = treatment_and_monitoring_links
-            table.insert(result_links, treatment_and_monitoring_header)
-        end
+        table.insert(result_links, documented_dx_header:build(true))
+        table.insert(result_links, alert_trigger_header:build(true))
+        table.insert(result_links, clinical_evidence_header:build(true))
+        table.insert(result_links, laboratory_studies_header:build(true))
+        table.insert(result_links, vital_signs_intake_header:build(true))
+        table.insert(result_links, treatment_and_monitoring_header:build(true))
+
         if existing_alert then
             result_links = links.merge_links(existing_alert.links, result_links)
         end
         Result.links = result_links
     end
+
 
 
     --------------------------------------------------------------------------------
