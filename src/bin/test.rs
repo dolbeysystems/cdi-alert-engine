44e15136d3f67baa7ea8b150ffc673877046f111
Evie <14899090+evie-calico@users.noreply.github.com>
2025-01-24 19:51:29 -0500

    Test cli

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,13 +1,28 @@
 use anyhow::{bail, Context};
 use cdi_alert_engine::cac_data::{self, Account, CdiAlert};
 use cdi_alert_engine::cdi_alerts;
+use clap::Parser;
 use mlua::LuaSerdeExt;
 use std::env;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use std::{fs, process::exit};
 use tracing::warn;
 
+#[derive(clap::Parser)]
+#[clap(author, version, about)]
+pub struct Cli {
+    /// Silence output of successful tests.
+    #[clap(short, long)]
+    pub quiet: bool,
+    #[clap(short, long, value_name = "path", default_value = "test.lua")]
+    pub config: Box<Path>,
+    /// If present, only run specified tests.
+    #[clap(value_name = "test")]
+    pub tests: Vec<String>,
+}
+
 fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
     tracing_subscriber::fmt::init();
 
     let (passed, failed, error) =
@@ -21,31 +36,48 @@ fn main() -> anyhow::Result<()> {
     let lua = cdi_alert_engine::make_runtime()?;
     let collections = fae_ghost::lib(&lua)?;
     let tests = lua
-        .load(fs::read_to_string("test.lua")?)
+        .load(fs::read_to_string(cli.config)?)
         .eval::<mlua::Table>()?;
 
     let mut passes = 0;
     let mut failures = 0;
 
     let mut scripts = tests
-        .pairs::<PathBuf, mlua::Table>()
-        .collect::<mlua::Result<Box<[(PathBuf, mlua::Table)]>>>()
+        .pairs()
+        .collect::<mlua::Result<Box<[(String, mlua::Table)]>>>()
         .with_context(|| "failed to collect test configurations")?;
     scripts.sort_unstable_by(|a, b| a.0.cmp(&b.0));
     for (script_path, accounts) in scripts {
+        let includes_script_path =
+            |x: &String| x.split_once(':').unwrap_or((x, "")).0 == script_path;
+        if !cli.tests.is_empty() && !cli.tests.iter().any(includes_script_path) {
+            continue;
+        }
         let script = lua
             .load(
                 fs::read_to_string(&script_path)
-                    .with_context(|| format!("failed to open {}", script_path.display()))?,
+                    .with_context(|| format!("failed to open {script_path}"))?,
             )
-            .set_name(format!("@{}", script_path.display()))
+            .set_name(format!("@{script_path}"))
             .into_function()
-            .with_context(|| format!("failed to load {} into lua", script_path.display()))?;
+            .with_context(|| format!("failed to load {script_path} into lua"))?;
         let accounts = accounts
-            .pairs::<PathBuf, mlua::Function>()
-            .collect::<mlua::Result<Box<[(PathBuf, mlua::Function)]>>>()
-            .with_context(|| format!("failed to collect accounts for {}", script_path.display()))?;
+            .pairs()
+            .collect::<mlua::Result<Box<[(String, mlua::Function)]>>>()
+            .with_context(|| format!("failed to collect accounts for {script_path}"))?;
         for (account_script_path, expected_result) in accounts {
+            if !cli.tests.is_empty()
+                && !cli
+                    .tests
+                    .iter()
+                    .filter(|x| includes_script_path(x))
+                    .any(|x| {
+                        x.split_once(':')
+                            .is_none_or(|(_, x)| x == account_script_path)
+                    })
+            {
+                continue;
+            }
             match test(
                 &lua,
                 &collections,
@@ -57,41 +89,27 @@ fn main() -> anyhow::Result<()> {
                     let result: CdiAlert = lua.globals().get("Result")?;
                     match expected_result.call::<(bool, Option<mlua::String>)>(result) {
                         Ok((true, _)) => {
-                            eprintln!(
-                                "{}:{} ... {passed}",
-                                script_path.display(),
-                                account_script_path.display()
-                            );
+                            if !cli.quiet {
+                                eprintln!("{script_path}:{account_script_path} ... {passed}",);
+                            }
                             passes += 1;
                         }
                         Ok((false, reason)) => {
-                            eprintln!(
-                                "{}:{} ... {failed}",
-                                script_path.display(),
-                                account_script_path.display()
-                            );
+                            eprintln!("{script_path}:{account_script_path} ... {failed}",);
                             if let Some(reason) = reason {
                                 eprintln!("\t{}", reason.display());
                             }
                             failures += 1;
                         }
                         Err(e) => {
-                            eprintln!(
-                                "{}:{} ... {error}",
-                                script_path.display(),
-                                account_script_path.display()
-                            );
+                            eprintln!("{script_path}:{account_script_path} ... {error}",);
                             eprintln!("\t{e}");
                             failures += 1;
                         }
                     }
                 }
                 Err(e) => {
-                    eprintln!(
-                        "{}:{} ... {error}",
-                        script_path.display(),
-                        account_script_path.display()
-                    );
+                    eprintln!("{script_path}:{account_script_path} ... {error}",);
                     eprintln!("\t{e}");
                     failures += 1;
                 }
@@ -99,7 +117,9 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    eprintln!();
+    if cli.quiet && failures > 0 || !cli.quiet && passes + failures > 0 {
+        eprintln!();
+    }
     eprintln!("{passes} test{} passed", if passes == 1 { "" } else { "s" });
     eprintln!(
         "{failures} test{} failed",
@@ -111,27 +131,24 @@ fn main() -> anyhow::Result<()> {
 fn test(
     lua: &mlua::Lua,
     collections: &mlua::Table,
-    script_path: &Path,
+    script_path: &str,
     script: &mlua::Function,
-    account_script_path: &Path,
+    account_script_path: &str,
 ) -> Result<(), anyhow::Error> {
     lua.load(
         fs::read_to_string(account_script_path)
-            .with_context(|| format!("failed to open {}", account_script_path.display()))?,
+            .with_context(|| format!("failed to open {account_script_path}"))?,
     )
-    .set_name(format!("@{}", account_script_path.display()))
+    .set_name(format!("@{account_script_path}"))
     .exec()
-    .with_context(|| format!("failed to evalutate {}", account_script_path.display()))?;
+    .with_context(|| format!("failed to evalutate {account_script_path}"))?;
     let Ok(accounts) = collections.get::<mlua::Table>("Accounts") else {
-        bail!("no accounts provided by {}", account_script_path.display());
+        bail!("no accounts provided by {account_script_path}");
     };
     let mut accounts = accounts.pairs::<mlua::Value, mlua::Value>();
     let (id, account) = accounts.next().unwrap()?;
     if accounts.next().is_some() {
-        warn!(
-            "multiple accounts provided by {}, using _id = {id:?}",
-            account_script_path.display()
-        );
+        warn!("multiple accounts provided by {account_script_path}, using _id = {id:?}",);
     }
     collections.clear()?;
     let mut account: Account = lua
@@ -140,7 +157,9 @@ fn test(
     cdi_alerts::build_account_caches(&mut account, 7, 7);
     let globals = lua.globals();
     globals.set("Account", account)?;
-    let script_name = script_path.file_name().map(|x| x.to_string_lossy());
+    let script_name = Path::new(script_path)
+        .file_name()
+        .map(|x| x.to_string_lossy());
     let script_name = script_name
         .as_ref()
         .map(|x| x.to_string())
@@ -154,5 +173,5 @@ fn test(
     )?;
     script
         .call::<()>(())
-        .with_context(|| format!("{} returned an error", script_path.display()))
+        .with_context(|| format!("{script_path} returned an error"))
 }
